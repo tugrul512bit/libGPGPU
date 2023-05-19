9eefaa7c1c5231b8a989b60ea7236d858bd76509
Hüseyin Tuğrul büyükışık <huseyin.tugrul.buyukisik@gmail.com>
2023-05-19 23:28:34 +0300

    test

--- main.cpp
+++ main.cpp
@@ -1,87 +1,89 @@
 
+// a program that adds 1 to all elements of an array
+// each GPU/Accelerator/CPU thread computes 4 element
+// both input and output arrays are integer arrays
+// 256k threads compute 1M elements
 
-#include <vector>
 #include <iostream>
-#include <regex>
-#include <fstream>
 #include "gpgpu.hpp"
 int main()
 {
     try
     {
-        constexpr size_t n = 1024*1024;
+        constexpr size_t n = 1024 * 1024;
+
+        // GPGPU::Computer::DEVICE_GPUS or _CPUS or _ACCS or "|" combination of them
+        // GPGPU::Computer::DEVICE_SELECTION_ALL gets all selected devices, 0 to N selects only 1 of them (0-based indexing)
         GPGPU::Computer computer(GPGPU::Computer::DEVICE_ALL, GPGPU::Computer::DEVICE_SELECTION_ALL);
 
+        // first compile a kernel (for now, it is in C99 language specs and compiled for every selected device)
         computer.compile(std::string(R"(
 
         #define n )") + std::to_string(n) + std::string(R"(
 
-            void kernel fmaTest(global int * a, global int * b) 
+            // every GPU thread adds one to 4 elements
+            void kernel add1ToEveryElementBut4ElementsPerThread(global int * a, global int * b) 
             {
-
                 // global id of this thread
                 const int id = get_global_id(0);
-
-       
-                b[id] =  a[id*4] + a[id*4+1] + a[id*4+2] + a[id*4+3];
+                const int dataStart = id*4;
+                for(int i=dataStart; i < dataStart + 4; i++)
+                {
+                    b[i] = a[i] + 1;
+                }
             }
-           )"), "fmaTest");
+           )"), "add1ToEveryElementBut4ElementsPerThread");
 
-        // true1: read mode (this is an input)
-        // false: write mode 
-        // true2: real whole array into device everytime a kernel with this parameter is run
-        // 1: number of elements per work-item / gpu-thread
-        // n: number of elements
-        // "a": name of array 
-        GPGPU::HostParameter a = computer.createHostParameter<int>("a", n, 4, true, false, false);
-        GPGPU::HostParameter b = computer.createHostParameter<int>("b", n, 1, false, true, false);
-        
+        // allocate input & output arrays, each with n integers, each with 4 elements per thread storage during load-balancing
+
+        GPGPU::HostParameter a = computer.createHostParameter<int>("a", n, 4, true /* read input elements from RAM to VRAM*/, false, false /* true: read all elements, false: read only own lanes */);
+        GPGPU::HostParameter b = computer.createHostParameter<int>("b", n, 4, false, true /* write output elements from VRAM to RAM */, false);
+
         for (int i = 0; i < n; i++)
         {
             a.access<int>(i) = i;
             b.access<int>(i) = 0;
         }
 
-        
-        computer.setKernelParameter("fmaTest", "a", 0);
-        computer.setKernelParameter("fmaTest", "b", 1);
-        
+        // set kernel parameters manually when not using parameter.next(parameter2).next(parameter3) .... inside compute() method of Computer struct
+        computer.setKernelParameter("add1ToEveryElementBut4ElementsPerThread", "a", 0);
+        computer.setKernelParameter("add1ToEveryElementBut4ElementsPerThread", "b", 1);
+
 
         constexpr int repeat = 100;
-        size_t nano, nano2 = 0;
+        size_t nano;
         {
 
             for (int i = 0; i < repeat; i++)
             {
                 {
+                    // benchmark this scope
                     Bench bench(&nano);
-                    //computer.compute(a.next(b), "fmaTest", 0, n / 4, 256);
-                    //computer.compute(a.next(b), "fmaTest", 0, n/4, 256,true,1024*8);
-                    //computer.run("fmaTest", 0, n/4, 256);
-                    computer.runFineGrainedLoadBalancing("fmaTest", 0, n/4, 256,1024*8);
-                }
-                nano2 += nano;
-                std::cout << "1 iteration = " << nano/1000000000.0 << " seconds" << std::endl;
 
+                    // multi-step load-balancing: slowly converges to balance, low latency per step
+                    computer.run("add1ToEveryElementBut4ElementsPerThread", 0, n / 4, 256); // n/4 number of total threads, 256 local threads per work group
+                    // this version does not need "setKernelParameter":
+                    //computer.compute(a.next(b), "add1ToEveryElementBut4ElementsPerThread", 0, n / 4, 256);
+
+                    // in-step load balancing: good starting balance, fair balancing for uneven workloads, high latency
+                    //computer.runFineGrainedLoadBalancing("add1ToEveryElementBut4ElementsPerThread", 0, n/4, 256,1024*8); // fine grain = 1024*8 threads (total work = 1M threads, divided into grains), 256 local threads (grain size has to be an integer multiple of local threads or at least equal to it)
+                    // this version does not need "setKernelParameter":
+                    //computer.compute(a.next(b), "add1ToEveryElementBut4ElementsPerThread", 0, n/4, 256,true,1024*8);
+
+                }
+                std::cout << "1 iteration = " << nano / 1000000000.0 << " seconds" << std::endl;
             }
 
         }
 
-
-        std::cout << "nanoseconds = " << nano2 << std::endl;
-        std::cout << "seconds = " << nano2 / 1000000000.0 << std::endl;
-        std::cout << "giga add-operations per second = " << ( ((size_t) repeat)  * n / (double)(nano2 / 1000000000.0)) / 1000000000.0 << std::endl;
-       
-        // sum of all sub-sums
-        size_t sum = 0;
-        size_t realSum = 0;
+        // check output to find any computational error of OpenCL or library
         for (int i = 0; i < n; i++)
-            realSum += i;
-        for (int i = 0; i < n/4; i++)
         {
-            sum += b.access<int>(i);
+            if (a.access<int>(i) + 1 != b.access<int>(i))
+                throw std::invalid_argument("error: opencl did not work!");
         }
-        std::cout << "sum = " << sum << " real sum ="<<realSum << std::endl;
+
+        std::cout << "ok" << std::endl;
     }
     catch (std::exception& ex)
     {
@@ -89,4 +91,5 @@ int main()
     }
     return 0;
 }
+
 
