30bcddd60c9281bfc96b9fc9d474d15c3161ddda
Hüseyin Tuğrul büyükışık <huseyin.tugrul.buyukisik@gmail.com>
2023-06-21 22:26:57 +0300

    test

--- gpgpu_init.hpp
+++ gpgpu_init.hpp
@@ -5,7 +5,7 @@
 #define CL_HPP_MINIMUM_OPENCL_VERSION 120
 #endif
 #define CL_HPP_TARGET_OPENCL_VERSION (120>CL_HPP_MINIMUM_OPENCL_VERSION?120:CL_HPP_MINIMUM_OPENCL_VERSION)
-#include "CL/opencl.hpp"
+#include <CL/opencl.hpp>
 #include <string>
 
 const char* getErrorString0(cl_int error);
